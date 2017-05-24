05475b6a4ca6a9336376c1f463d91835f8957c65
Mr.V <2671440661@qq.com>
2017-05-25 02:01:56 +0800

    build.sh

--- Main/main.cpp
+++ Main/main.cpp
@@ -8,11 +8,11 @@ int main(int arg, char **args) {
     using namespace std;
     using namespace cv;
     CurlModel curl;
-    string weather = curl.getData("http://www.weather.com.cn/data/cityinfo/101270101.html");
+    string weather =curl.getData("http://www.weather.com.cn/data/cityinfo/101270101.html");
     if (weather != "") {
         cout << weather << endl;
     } else {
-        weather =="{'Weather':'No internet'}";
+    weather ="{'Weather':'No internet'}";
     }
     videoThread video;
     try {
--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ mkdir $1
 cd $1
 cmake ..
 make -j8
+mkdir bin/temp
+echo "" >> bin/temp/data
+cp -r ../haarcascades bin/
