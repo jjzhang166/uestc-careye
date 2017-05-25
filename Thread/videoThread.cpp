11aa212a6c76c41cafef9cd8b33c4219bd847898
Mr.V <2671440661@qq.com>
2017-05-26 01:48:53 +0800

    debug

--- Thread/videoThread.cpp
+++ Thread/videoThread.cpp
@@ -14,7 +14,7 @@ void videoThread::startVideo(int cap_1, int cap_2, int width, int hight, string 
     cout<<"open cap_2"<<endl;
     imageController img_c;
     bool cap_open=(cap_1_1.isOpened()) && (cap_1_2.isOpened());
-    cout<<"open status"<<"---------------->"<<cap_open<<endl;
+    cout<<"open status"<<"---------------->"<<(cap_open==1?"成功":"失败")<<endl;
     if (cap_open) {
         while (1) {
             cap_1_1 >> left;
@@ -23,8 +23,8 @@ void videoThread::startVideo(int cap_1, int cap_2, int width, int hight, string 
             //dec.detcetBody(left);
             //TODO:debug 异常数据right摄像头
             //dec.detcetBody(left);
-            Mat merge=img_c.image2Merge(left, right, width, hight,weather);
-            imshow("merge", merge);
+            //Mat merge=img_c.image2Merge(left, right, width, hight,weather);
+            imshow("merge", left);
             if (char(waitKey(1)) == 'q') {
                 break;
             }
