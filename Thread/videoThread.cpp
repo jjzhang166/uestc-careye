345bb78412b9e4e15916750f001724574e424955
Mr.V <2671440661@qq.com>
2017-05-26 00:52:07 +0800

    debug

--- Thread/videoThread.cpp
+++ Thread/videoThread.cpp
@@ -8,9 +8,12 @@ void videoThread::startVideo(int cap_1, int cap_2, int width, int hight, string 
     Mat right;
     VideoCapture cap_1_1, cap_1_2;
     cap_1_1.open(cap_1);
+    cout<<"open cap_1"<<endl;
     cap_1_2.open(cap_2);
+    cout<<"open cap_2"<<endl;
     imageController img_c;
     bool cap_open=(cap_1_1.isOpened()) && (cap_1_2.isOpened());
+    cout<<"open status"<<"---------------->"<<cap_open<<endl;
     if (cap_open) {
         while (1) {
             cap_1_1 >> left;
