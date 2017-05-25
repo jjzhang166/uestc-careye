1e2bfe2b44d1eb288ebcd0833e3a902fb9134f77
Mr.V <2671440661@qq.com>
2017-05-26 01:46:46 +0800

    debug

--- Thread/videoThread.cpp
+++ Thread/videoThread.cpp
@@ -18,11 +18,11 @@ void videoThread::startVideo(int cap_1, int cap_2, int width, int hight, string 
     if (cap_open) {
         while (1) {
             cap_1_1 >> left;
-            cap_1_2 >> right;///
+            cap_1_2 >> right;
             Detcet dec;
-            dec.detcetBody(left);
+            //dec.detcetBody(left);
             //TODO:debug 异常数据right摄像头
-            dec.detcetBody(left);
+            //dec.detcetBody(left);
             Mat merge=img_c.image2Merge(left, right, width, hight,weather);
             imshow("merge", merge);
             if (char(waitKey(1)) == 'q') {
