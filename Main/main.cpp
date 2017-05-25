5bc534d738f178ec8de9d5e18885c056cace3895
Mr.V <2671440661@qq.com>
2017-05-26 01:04:46 +0800

    debug

--- Main/main.cpp
+++ Main/main.cpp
@@ -7,18 +7,33 @@
 int main(int arg, char **args) {
     using namespace std;
     using namespace cv;
-    CurlModel curl;
-    string weather =curl.getData("http://www.weather.com.cn/data/cityinfo/101270101.html");
-    if (weather != "") {
-        cout << weather << endl;
-    } else {
-    weather ="{'Weather':'No internet'}";
+    Mat left;
+    Mat right;
+    int cap_1=0;
+    VideoCapture cap_1_1;
+    cap_1_1.open(cap_1);
+    cout<<"open cap_1"<<endl;
+    bool cap_open=(cap_1_1.isOpened());
+    cout<<"open status"<<"---------------->"<<cap_open<<endl;
+    while (1) {
+        cap_1_1 >> left;
+        imshow("merge", left);
+        if (char(waitKey(1)) == 'q') {
+            break;
+        }
     }
-    videoThread video;
-    try {
-        video.startVideo(0, 1, 640, 480,weather);
-    } catch (Exception e) {
-        throw new Exception();
-    }
+//    CurlModel curl;
+//    string weather =curl.getData("http://www.weather.com.cn/data/cityinfo/101270101.html");
+//    if (weather != "") {
+//        cout << weather << endl;
+//    } else {
+//    weather ="{'Weather':'No internet'}";
+//    }
+//    videoThread video;
+//    try {
+//        video.startVideo(0, 1, 640, 480,weather);
+//    } catch (Exception e) {
+//        throw new Exception();
+//    }
     return 0;
 }
