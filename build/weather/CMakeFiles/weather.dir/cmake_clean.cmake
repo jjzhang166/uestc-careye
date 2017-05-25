bff55c2adffaffc52b3273f769921ecdb0c7278c
Mr.V <2671440661@qq.com>
2017-05-26 01:37:37 +0800

    debug

---
+++ build/weather/CMakeFiles/weather.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/weather.dir/CurlModel.cpp.o"
+  "CMakeFiles/weather.dir/weatherApi.cpp.o"
+  "CMakeFiles/weather.dir/HTTP.cpp.o"
+  "../bin/libs/libweather.pdb"
+  "../bin/libs/libweather.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/weather.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
